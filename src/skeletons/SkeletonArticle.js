c89a4b32f83923c87e795f176363ae61dcfea457
Ninja <cuansenputra@gmail.com>
2022-11-15 17:13:18 +0700

    fetching data && loading animation template

---
+++ src/skeletons/SkeletonArticle.js
@@ -0,0 +1,19 @@
+import SkeletonElement from "./SkeletonElement";
+
+
+const SkeletonArticle = () => {
+    return ( 
+    <div className="skeleton-wrapper">
+        <div className="skeleton-article">
+            <SkeletonElement type="title" />
+            <SkeletonElement type="text" />
+            <SkeletonElement type="text" />
+            <SkeletonElement type="text" />
+
+            
+        </div>
+    </div> 
+    );
+}
+ 
+export default SkeletonArticle;
